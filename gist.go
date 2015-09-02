0c850f1160bd0f374a25528db27e789185d457a3
178inaba <178inaba@users.noreply.github.com>
2015-09-02 18:12:30 +0900

    add create gist

--- gist.go
+++ gist.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 
+	"golang.org/x/oauth2"
+
 	log "github.com/Sirupsen/logrus"
+	"github.com/google/go-github/github"
 )
 
 func init() {
@@ -13,12 +17,53 @@ func init() {
 func gistHandler(w http.ResponseWriter, r *http.Request) {
 	log.Debug("gistHandler()")
 
+	// get access token
 	c, err := r.Cookie("access_token")
 	if err != nil {
 		log.Error("get cookie error: ", err)
+		return
 	}
 
-	log.Debug(c.Value)
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: c.Value},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
 
-	// TODO(178inaba): create gist
+	// github api
+	client := github.NewClient(tc)
+
+	// get post source
+	byteBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Error("body read error: ", err)
+	}
+
+	body := string(byteBody)
+
+	// create post gist
+	postGist := github.Gist{
+		Description: getStrPtr("test gist"),
+		Public:      getBoolPtr(false),
+		Files: map[github.GistFilename]github.GistFile{
+			"test.go": github.GistFile{
+				Content: &body,
+			},
+		},
+	}
+	retGist, resp, err := client.Gists.Create(&postGist)
+	if err != nil {
+		log.Error("create gist error: ", err)
+		return
+	}
+
+	log.Debug(retGist)
+	log.Debug(resp)
+}
+
+func getStrPtr(s string) *string {
+	return &s
+}
+
+func getBoolPtr(b bool) *bool {
+	return &b
 }
