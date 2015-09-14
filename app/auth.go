ad65b35ac1aa1e7827d92471d69955370e1c758d
178inaba <magic.brain.evolution.7.0@gmail.com>
2015-09-14 20:20:36 +0900

    add save snip id

--- app/auth.go
+++ app/auth.go
@@ -15,8 +15,9 @@ func init() {
 func authHandler(w http.ResponseWriter, r *http.Request) {
 	log.Debug("authHandler()")
 
-	// get code
 	r.ParseForm()
+
+	// get code
 	_, ok := r.Form["code"]
 	if !ok {
 		log.Error("auth code not found error")
@@ -31,14 +32,29 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// access token
+	// access token save in cookie
 	log.Debugf("access token: %s", tok.AccessToken)
 
-	c := http.Cookie{
+	http.SetCookie(w, &http.Cookie{
 		Name:  "access_token",
 		Value: tok.AccessToken,
+	})
+
+	// get snip id
+	_, ok = r.Form["snip_id"]
+	if !ok {
+		log.Error("snip id not found error")
+		http.Error(w, "Server Error", http.StatusInternalServerError)
+		return
 	}
-	http.SetCookie(w, &c)
+
+	// snip id save in cookie
+	log.Debugf("snip id: %s", r.Form["snip_id"][0])
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "snip_id",
+		Value: r.Form["snip_id"][0],
+	})
 
 	http.Redirect(w, r, "/", http.StatusFound)
 }
